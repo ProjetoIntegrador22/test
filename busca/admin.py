6bf0eeb069bb33c4b66a11afbd9211f125b34df5
Diogo <digo.pasi@gmail.com>
2024-04-01 13:56:09 -0300

    Models e Admin

--- busca/admin.py
+++ busca/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from busca import models
 
 # Register your models here.
+
+class VisualPreco(admin.ModelAdmin):
+    list_display = ('preco', 'ultima_atualizacao')
+
+class VisualProvedores(admin.ModelAdmin):
+    list_display = ('id', 'apelido', 'cnpj')
+    list_display_links = ('id', 'apelido')
+
+
+
+admin.site.register(models.Provedores, VisualProvedores)
+admin.site.register(models.Disponibilidade)
+admin.site.register(models.Servicos)
+admin.site.register(models.PrecoServicos, VisualPreco)
--- busca/models.py
+++ busca/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Provedores(models.Model):
+    nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=20)
+    cnpj = models.DecimalField(max_digits=14, decimal_places=0)
+
+    class Meta:
+        verbose_name_plural = 'Provedores'
+
+    def __str__(self):
+        return self.apelido
+
+class Servicos(models.Model):
+    provedor = models.ManyToManyField(Provedores)
+    nome = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = 'Servicos'
+
+    def __str__(self):
+        return self.nome
+
+class Disponibilidade(models.Model):
+    servico = models.ForeignKey(Servicos, on_delete=models.CASCADE)
+    data = models.DateField()
+    local = models.DecimalField(max_digits=8, decimal_places=0)
+
+    def __str__(self):
+        return self.servico
+    
+class PrecoServicos(models.Model):
+    servico = models.ManyToManyField(Servicos)
+    provedor = models.ManyToManyField(Provedores)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    ultima_atualizacao = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name_plural = 'PrecoServicos'
+
+    
